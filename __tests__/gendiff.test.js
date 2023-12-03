93b14e673155e0a405b54a24a57cc69f946b4755
Ignatii1 <qinfen@ya.ru>
2023-12-04 02:31:06 +0400

    finished json plain

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,22 +1,56 @@
 const { execSync } = require('child_process');
 const path = require('path');
+const yaml = require('js-yaml');
+const fs = require('fs');
 
 describe('gendiff', () => {
-  const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const getFileFormat = (filename) => {
+    return filename.endsWith('.yaml') || filename.endsWith('.yml') ? 'yaml' : 'json';
+  }
+  const yamlToJson = (filepath) => {
+    try {
+      const fileContents = fs.readFileSync(filepath, 'utf8');
+      return yaml.load(fileContents);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
   const runGendiff = (filename1, filename2) => {
+    const format1 = getFileFormat(filename1);
+    const format2 = getFileFormat(filename2);
+
     const filepath1 = getFixturePath(filename1);
     const filepath2 = getFixturePath(filename2);
-    return execSync(`node ./gendiff.js ${filepath1} ${filepath2}`).toString();
+
+    const file1 = format1 === 'yaml' ? yamlToJson(filepath1) : require(filepath1);
+    const file2 = format2 === 'yaml' ? yamlToJson(filepath2) : require(filepath2);
+
+    return execSync(`node ./gendiff.js ${file1} ${file2}`).toString();
   };
 
   test('compares two flat JSON files with no differences', () => {
     const result = runGendiff('file1.json', 'file1.json');
-    expect(result).toBe('{\n   // expected output when files are identical\n}');
+    expect(result).toBe(`{
+   follow: false
+   host: hexlet.io
+   proxy: 123.234.53.22
+   timeout: 50
+}\n`);
   });
+
 
   test('compares two flat JSON files with differences', () => {
     const result = runGendiff('file1.json', 'file2.json');
-    expect(result).toBe('{\n   // expected output when files are different\n}');
+    expect(result).toBe(`{
+ - follow: false
+   host: hexlet.io
+ - proxy: 123.234.53.22
+ - timeout: 50
+ + timeout: 20
+ + verbose: true
+}\n`);
   });
 
 });
--- gendiff.js
+++ gendiff.js
@@ -8,50 +8,90 @@ var _ = require('lodash')
 
 const program = new Command();
 
-const parsers = {
-  '.json': JSON.parse,
-  '.yaml': undefined,
-  '.yml': undefined,
-}
-
 const fileData = (filepath) => {
   const absolutePath = path.resolve(filepath);
-  const fileExtestion = path.extname(absolutePath);
-  const parser = parsers[fileExtestion];
-  if (!parser) {
-    throw new Error("Unsupported file format: ${fileExtension}");
-  }
   const data = fs.readFileSync(absolutePath, 'utf-8');
-  return parser(data)
+  return JSON.parse(data);
 }
 
 program
   .version(packageJson.version)
   .description("Compares two configuration files and shows a difference.")
-  .option("-f, --format <type>", "output format")
+  .option("-f, --format <type>", "output format", 'stylish')
   .argument('<filepath1>', 'path to the first source')
   .argument('<filepath2>', 'path to the second source')
-  .action((filepath1, filepath2) => {
-    let result = '{\n';
+  .action((filepath1, filepath2, options) => {
+    const fileData = (filepath) => {
+      const absolutePath = path.resolve(filepath);
+      const data = fs.readFileSync(absolutePath, 'utf-8');
+      return JSON.parse(data);
+    };
+    const formatPlain = (file1, file2) => {
+      let result = [];
+      const keys = _.union(_.keys(file1), _.keys(file2));
+
+      keys.forEach(key => {
+        if (!_.has(file2, key)) {
+          result.push(`Property '${key}' was removed`);
+        } else if (!_.has(file1, key)) {
+          result.push(`Property '${key}' was added with value: ${JSON.stringify(file2[key])}`);
+        } else if (file1[key] !== file2[key]) {
+          result.push(`Property '${key}' was updated. From ${JSON.stringify(file1[key])} to ${JSON.stringify(file2[key])}`);
+        }
+      });
+
+      return result.join('\n');
+    };
+
+    const formatJson = (file1, file2) => {
+      const diff = {};
+      const keys = _.union(_.keys(file1), _.keys(file2));
+
+      keys.forEach(key => {
+        if (!_.has(file2, key)) {
+          diff[key] = { status: 'removed', oldValue: file1[key] };
+        } else if (!_.has(file1, key)) {
+          diff[key] = { status: 'added', newValue: file2[key] };
+        } else if (file1[key] !== file2[key]) {
+          diff[key] = { status: 'updated', oldValue: file1[key], newValue: file2[key] };
+        } else {
+          diff[key] = { status: 'unchanged', value: file1[key] };
+        }
+      });
+
+      return JSON.stringify(diff, null, 2);
+    };
+
+    const formatOutput = (file1, file2, format) => {
+      switch (format) {
+        case 'plain':
+          return formatPlain(file1, file2);
+        case 'stylish':
+        default:
+          // здесь должна быть логика для стандартного форматирования
+          return formatStylish(file1, file2);
+      }
+    };
+
+
     const file1 = fileData(filepath1);
     const file2 = fileData(filepath2);
-    const allKeys = new Set([...Object.keys(file1), ...Object.keys(file2)]);
-    const sortedKeys = _.orderBy(Array.from(allKeys));
+    const format = options.format || 'stylish';
 
-    for (const key of sortedKeys) {
-      if (file1[key] === file2[key]) {
-        result += `   ${key}: ${file1[key]}\n`
-      } else {
-        if (file1.hasOwnProperty(key)) {
-          result += ` - ${key}: ${file1[key]}\n`
-        }
-        if (file2.hasOwnProperty(key)) {
-          result += ` + ${key}: ${file2[key]}\n`
-        }
-      }
+    let result;
+    switch (format) {
+      case 'json':
+        result = formatJson(file1, file2);
+        break;
+      case 'plain':
+        result = formatPlain(file1, file2);
+        break;
+      case 'stylish':
+      default:
+        result = formatStylish(file1, file2);
     }
-    result += '}'
-    console.log(result)
+
+    console.log(result);
   });
 
 
